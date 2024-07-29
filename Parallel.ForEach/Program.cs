61ceb9cc5fb367ec422b62da0562c2c3dfd9f3ad
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-29 16:14:30 +0200

    Add project files.

---
+++ Parallel.ForEach/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> urls = new List<string>
+{
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/comments",
+    "https://jsonplaceholder.typicode.com/albums"
+};
+
+Parallel.ForEach(urls, url =>
+{
+     Console.WriteLine($"Fetchind data from {url} on thread {Task.CurrentId}");
+});
+Console.WriteLine("All tasks completed.");
